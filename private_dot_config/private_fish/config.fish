279478633e0b419903f1ff8425529c00b04b3e33
JP3BGY <6635381+JP3BGY@users.noreply.github.com>
2022-05-28 02:04:22 +0900

    autostart zellij

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,4 +1,16 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -x ZELLIJ_AUTO_EXIT true
+    if not set -q ZELLIJ
+        if test "$ZELLIJ_AUTO_ATTACH" = "true"
+            zellij attach -c
+        else
+            zellij
+        end
+
+        if test "$ZELLIJ_AUTO_EXIT" = "true"
+            kill $fish_pid
+        end
+    end
 end
 
